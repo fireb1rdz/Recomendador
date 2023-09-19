71ae49dd46436d574159d807b28195f57f3fffbc
BrunoFerreira1990 <111455462+BrunoFerreira1990@users.noreply.github.com>
2023-09-19 18:34:21 -0300

    Finalização da função de criar filtro por categoria

--- Recomendador.py
+++ Recomendador.py
@@ -1,7 +1,7 @@
 filmes = {
     1: {"titulo": "Matrix", "categoria": "Ação", "avaliacao": 4.5, "favorito": False, "assistido": False},
     2: {"titulo": "Interestelar", "categoria": "Ficção Científica", "avaliacao": 4.8, "favorito": False, "assistido": False},
-    3: {"gtitulo": "Pantera Negra", "categoria": "Ação", "avaliacao": 4.3, "favorito": False, "assistido": False},
+    3: {"titulo": "Pantera Negra", "categoria": "Ação", "avaliacao": 4.3, "favorito": False, "assistido": False},
     4: {"titulo": "Cidade de Deus", "categoria": "Drama", "avaliacao": 4.7, "favorito": False, "assistido": False},
     5: {"titulo": "Breaking Bad", "categoria": "Drama", "avaliacao": 4.9, "favorito": False, "assistido": False},
     6: {"titulo": "Vingadores: Ultimato", "categoria": "Ação", "avaliacao": 4.9, "favorito": False, "assistido": False},
@@ -29,6 +29,42 @@ filmes = {
 
 def listar_filmes():
     for filme_id, filme_info in filmes.items():
-        print(
-            f"ID: {filme_id} - Título: {filme_info['titulo']} - Categoria: {filme_info['categoria']} - Avaliação da crítica: {filme_info['avaliacao']}")
+        print(f"ID: {filme_id} - Título: {filme_info['titulo']} - Categoria: {filme_info['categoria']} - Avaliação da crítica: {filme_info['avaliacao']}")
 
+
+
+
+
+
+
+
+
+
+
+def filtrar_categoria(filtrar: str):
+    """
+    Esta função filtra os filmes por categoria.
+
+    Args:
+        filtrar (string): Informa a categoria de filme a ser buscado na listagem dos filmes.
+
+    Returns:
+        string: Puxa as informações dos filmes de acordo com a categoria digitada,
+
+    Exemplo:
+        filtrar_categoria(drama)
+        ID: 4 - Título: Cidade de Deus - Categoria: Drama - Avaliação: 4.7
+    """
+
+    for chave, valor in filmes.items():
+
+        if filtrar in valor['categoria']:
+            print(f"ID: {chave} - Título: {valor['titulo']} - Categoria: {valor['categoria']} - Avaliação: {valor['avaliacao']}")
+
+        elif filtrar not in ["Ação", "Drama", "Comédia", "Ficção Científica", "Animação", "Terror", "Romance", "Fantasia", "Suspense", "Documentário"]:
+            print("A categoria digitada não consta na relação dos filmes.")
+            break
+
+
+filtrar = input("Digite a categória que você deseja filtrar: ").title()
+filtrar_categoria(filtrar)
